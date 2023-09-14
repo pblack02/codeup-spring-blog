ed595fcbd2e8bc7b00be1277702a55393843cc90
Presley Black <presleyblack24@gmail.com>
2023-09-14 15:08:07 -0500

    first commit

---
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package edu.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/world")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+}
